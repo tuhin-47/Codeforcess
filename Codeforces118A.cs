fbccbe9f78c53e1c2eb8d1a2c58487a7f9619829
Hasan Mahumd   (Tuhin) <tuhin.mc@gmail.com>
2023-07-31 17:40:20 +0600

    Create Codeforces118A.cs

---
+++ Codeforces118A.cs
@@ -0,0 +1,29 @@
+/**
+ Problem Link : https://codeforces.com/problemset/problem/118/A
+ Status : Accepted.
+
+Sample input : tour
+Sample Output : .t.r
+
+**/
+
+namespace Codeforces118A
+{
+    internal class Codeforces118AClass
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            word = word.ToLower();
+
+            for(int i = 0; i < word.Length; i++)
+            {
+                char ch = word[i];
+
+                if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u' && ch!= 'y')
+                    Console.Write(".{0}", ch);
+            }
+            Console.WriteLine();
+        }
+    }
+}
